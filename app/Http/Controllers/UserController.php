d748a33f21e19a25aa02d506cb0f78ca39c8bd6c
ShimizuKazuki <shimizu@posl.ait.kyushu-u.ac.jp>
2021-05-04 00:28:43 +0900

    add validate

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,11 +6,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Requests\UserRegistPost;
 
 class UserController extends Controller{
-
     //引数でUserRegistPostクラスのインスタンスを渡す
     public function register(UserRegistPost $request){
+        $rules = [
+            'name' => ['required', 'max:20'],
+            'email' => ['required', 'email', 'max:255'],
+        ];
+        //全ての入力値を取得し保持
+        $inputs = $request->all();
+
+        //validatorクラスのインスタンスを作成
+        $validator = Validator::make($input, $rules);
+        if($validator->fails()){
+            //エラー処理
+        }
+
         //インスタンスに対して値を問い合わせ
         $name = $request->input('name');
-        $name = $request->input('age');
+        $email = $request->input('email');
     }
 }
